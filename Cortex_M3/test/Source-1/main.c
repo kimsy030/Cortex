be62bee571acc2c62bb5d1e845b982af6c9b8daa
kimsy030 <201605ksy@dsm.hs.kr>
2022-05-24 13:46:16 +0900

    Update and rename Cortex_M3/test/main.c to Cortex_M3/test/Source-1/main.c

--- Cortex_M3/test/Source-1/main.c
+++ Cortex_M3/test/Source-1/main.c
@@ -11,4 +11,12 @@ int main(void)
            HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET); // 2, 3, 4번 LED 소등
            HAL_Delay(500);
       }
+      ======================================================================================================================
+      while (1)
+      {
+           HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4);
+           HAL_Delay(500); // ON일 때 500, OFF일 때 500 Delay
+      }
+      
+      // GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4 = (4  + 2 + 1) * 4
 }
